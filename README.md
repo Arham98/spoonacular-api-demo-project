070c6e07243735ae2cd763f85c3633dd204b9e38
Arham Saeed <saeedarham100@gmail.com>
2023-02-11 16:58:29 +0500

    Logo & Loader Updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # spoonacular-api-demo-project
- A demo React-Node application showing the integration of the Spoonacular API
+ A demo React-Node application showing the integration of the Spoonacular API.
--- spoonacular-app/src/components/utilities/Loading.jsx
+++ spoonacular-app/src/components/utilities/Loading.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Spinner from 'react-bootstrap/Spinner';
 
 function Loading() {
   return (
     <Container fluid>
       <Container className="center-container">
         <div>
-          <Spinner className="spinner-border-1" animation="grow" style={{ color: '#01B636' }} />
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Spinner className="spinner-border-2" animation="grow" style={{ color: '#01B636' }} />
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Spinner className="spinner-border-3" animation="grow" style={{ color: '#01B636' }} />
+          <div className="loader">
+            <div className="inner one" />
+            <div className="inner two" />
+            <div className="inner three" />
+          </div>
         </div>
       </Container>
     </Container>
